1de2575df9d86e6d5c4003123e265c994988a7a8
IlhanAfandiyev <efendiyevilhanps4@gmail.com>
2025-06-20 13:57:52 +0400

    modals added

--- src/Pages/Home/Parts/Main/index.tsx
+++ src/Pages/Home/Parts/Main/index.tsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from "./style.module.css"
 import Doner from "../../../../assets/imgs/beautiful-traditional-turkish-doner-cooked-in-a-hot-fire-video.jpg"
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const HomeMain = () => {
+  const [showModal, setShowModal] = useState(false)
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    const user = localStorage.getItem("User")
+    if (user) {
+      navigate("/restaurant")
+    } else {
+      setShowModal(true)
+    }
+  }
+
   return (
     <div className='container'>
       <div className={style.CardContainer}>
-        <Link to="/restaurant" className={style.FirstCard}>
+        <div className={style.FirstCard} onClick={handleClick}>
           <div className={style.imageContainer}>
             <img src={Doner} alt="" />
             <p>Seidin Doneri</p>
           </div>
-        </Link>
-        <Link to="/restaurant" className={style.FirstCard}>
+        </div>
+        <div className={style.FirstCard} onClick={handleClick}>
           <div className={style.imageContainer}>
             <img src={Doner} alt="" />
             <p>Seidin Doneri</p>
           </div>
-        </Link>
+        </div>
       </div>
+
+      {showModal && (
+        <div className={style.modalOverlay}>
+          <div className={style.modal}>
+            <p className={style.modalText}>You must be logged in to view this restaurant.</p>
+            <button onClick={() => setShowModal(false)} className={style.modalButton}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
